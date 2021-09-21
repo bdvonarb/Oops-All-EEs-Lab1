de99e4bd206cf9988fea5183e20d6756e48dd5ab
Jaxen <shawvannatta@uiowa.edu>
2021-09-21 10:24:51 -0500

    set up temp and time vectors

--- desktop/main.py
+++ desktop/main.py
@@ -1,6 +1,11 @@
+
 import tkinter as tk
 import matplotlib.pyplot as plt
+#import matplotlib.figure as Figure
 import numpy as np
+from matplotlib.animation import FuncAnimation
+import random as rd
+import tkinter.font as tkFont
 
 from tkinter import ttk
 from tkinter import *
@@ -14,9 +19,30 @@ window.geometry("750x750")
 button_remote = tk.Button(window, text = "Display on Remote",height = 15 ,width = 30)
 button_remote.place(x="100",y="450")
 
-button_temp = tk.Button(window, text = "TEMP",height = 15 ,width = 30)
+button_temp = tk.Button(window, text = "",height = 15 ,width = 30)
 button_temp.place(x="450",y="450")
 
+I=True
+time_s = [-300]
+temp = [0]
+for x in range (0, 300):
+    time_s.append(x-299)
+    if x>0:
+        temp.append(0)
+
+siz=tkFont.Font(size=30)
+
+temp_=tk.Label(window, text = temp[len(temp)-1], font= siz)
+temp_.place(x="537",y="545")
+
+
+#def animate(i):
+#    plt.cla()
+#    plt.plot(time_s,temp)
+
+#ani = FuncAnimation(plt.gcf(),animate)
+
+#plt.show()
 
 
 

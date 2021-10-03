7f890151f4e3f1301fff70db01f330ca51c44b40
Jaxen <shawvannatta@uiowa.edu>
2021-10-03 09:57:56 -0500

    Commented, took out libraries that were not needed

--- desktop/main.py
+++ desktop/main.py
@@ -1,21 +1,12 @@
 from numpy.core.numeric import NaN
-from numpy.lib.function_base import diff
 from password import THINGSPEAK_API_READKEY, THINGSPEAK_CHANNELID, THINGSPEAK_API_WRITEKEY
 import tkinter as tk
 import numpy as np
 import matplotlib.figure as fi
-import matplotlib.animation as ani
-from matplotlib.animation import FuncAnimation
-import random as rd
 import tkinter.font as tkFont
 import requests
-import json
-import math
-import time
-import threading
 import datetime
 
-from tkinter import ttk
 from tkinter import *
 from tkinter.ttk import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg   
@@ -122,50 +113,51 @@ def update():
 
 #function for graphing the temp vector vs the time vector
 def update_g():
-    #width = int(window.winfo_screenmmwidth())
-    #height = int(window.winfo_screenheight())
 
-    #width = int(window.winfo_reqwidth())
-    #height = int(window.winfo_reqheight())
-
+    #pull the size of the window and set empty vectors for width and height
     wbyh = str(window.geometry())
     width=[]
     height=[]
 
-    check=0
+    check=0 #check to see if we hit x because the size is in the form 000x000 which shows the transition to height
     for x in range(0,len(wbyh)):
+        #check if the value in the string is a number and see if we have crossed the x yet
         if wbyh[x].isnumeric() and check==0:
             width.append(int(wbyh[x]))
-
+        #if we have crossed the x then only add numbers to the height vector
         if wbyh[x].isnumeric() and check==1:
             height.append(int(wbyh[x]))
-
+        #ensure that nothing is done when the for loop hits the x and assign check to 1 so we can append to the height
         elif wbyh[x]=="x":
             check=1
-
+        #once we hit the + sign we have recieved all the information we want about the dimensions of the window
         elif wbyh[x]=="+":
             break
 
-    nwidth=""
+    nwidth=""#create an empty string for width to convert out of vector form (side note this could have probably been compressed with the code above)
+    #go through the length of the width vector and convert it to a string
     for x in range(0,len(width)):
         nwidth+=str(width[x])
     
+    #same as just above but for height
     nheight=""
     for x in range(0,len(height)):
         nheight+=str(height[x])
     
 
 
-    fig.set_size_inches((int(nwidth)/1000)*8,(int(nheight)/1000)*5.73)
+    fig.set_size_inches((int(nwidth)/1000)*8,(int(nheight)/1000)*5.73)#function that has the size of the garph set as a ratio based on the size of the window
 
     temp_figure = fig.add_subplot(111) #assigning the figure a plot 
-    temp_figure.clear()
+    temp_figure.clear()#clear the plot or we get graphs overlapping on each other
     temp_figure.plot(time_s,temp, color= "black") #plotting time vs temp
+
+    #set information about the graph
     temp_figure.set_title("Temperature")
     temp_figure.set_xlabel("Last 300 Seconds")
     temp_figure.set_xbound(lower=-300,upper=0)
     
-    #depending on the state of a being triggered tells us if temp is being scaled in which case we need to switch the y axis accordingly
+    #depending on the state of "a" being triggered tells us if temp is being scaled in which case we need to switch the y axis accordingly
     if a==1:
         temp_figure.set_ylabel("Degrees Fahrenheit")
         temp_figure.set_ybound(lower=50,upper=122)
@@ -173,14 +165,14 @@ def update_g():
         temp_figure.set_ylabel("Degrees Celsius")
         temp_figure.set_ybound(lower=10,upper=50)
 
-    #displaying the plot to the window via the figure object
+    #this for loop goes through all of the widgets every second and destroys all but the most recent canvas object in order to not have Canvases overlapping when were trying to resize the plot
     for widget in window.winfo_children():
         if widget.widgetName == "canvas":
             widget.destroy()
 
+    #displaying the plot to the graph object via the the figure object
     graph = FigureCanvasTkAgg(fig,master= window)
     graph.get_tk_widget().place(x=75,y=0)
-
     graph.draw()
     
 
@@ -190,8 +182,9 @@ def update_g():
 def update_v():
     r = requests.get("https://api.thingspeak.com/channels/"+ THINGSPEAK_CHANNELID + "/fields/1/last.json", params={"api_key":THINGSPEAK_API_READKEY})#request most recent data point
     j = r.json()#assign data to vector j
-    global ID_value
-    global a
+
+    global ID_value#ID variable to determine if we are indeed recieving new information on every request for new information
+    global a #the variable that represents if we are in Farenheit or Celsiu, basically if the change temp button has been pressed
 
     compare_to_utc=str(datetime.datetime.utcnow()) #pull the current UTC time
 
@@ -205,7 +198,7 @@ def update_v():
     t_reading=t_reading[-9:]
     t_reading=int(t_reading[0:2])*3600+int(t_reading[3:5])*60+int(t_reading[6:8])
     
-    diff_t=compare_to_utc-t_reading #compare the two to determine how long since a new measurement
+    diff_t=compare_to_utc-t_reading #compare utc time and system time to determine the length of time that has elapsed since a new reading
 
     #compare the length of time to a new measurement and determine if its been longer then 3 seconds, if not execute as long as the most recent value is not NaN
     if diff_t<3 and not np.isnan(float(j["field1"])):
@@ -219,46 +212,52 @@ def update_v():
                 temp[299]=(float(j["field1"])*1.8)+32
             else:
                 temp[299]=float(j["field1"])
-        update()
+        update()#update plot and temp reading
     
+    #if the temperatur reading is NaN then I know that the probe has been unplugged because thats what we coded as the response to the backend
     elif np.isnan(float(j["field1"])):
+
+        #for loop that updates all the value locations
         for x in range(0,299):
             temp[x]=temp[x+1]
-        temp[299]=NaN        
+        temp[299]=NaN #assign NaN to the most recent reading and display probe error for the temp reading       
         temp_.config(text="Probe Er")
         temp_.place(x="480",y="545")
-        fig.clf()
-        update_g()
+        fig.clf()#clear figure or plots overlap
+        update_g()#update plot
 
+    #else statement that catches if the box has been turned off because our difference in UTC and system time are different
     else:
         for x in range(0,299):
             temp[x]=temp[x+1]
         temp[299]=NaN
-        update()
+        update()#update plot and temp reading
 
+    window.after(1000,update_v)#keep us in this loop until the window has been closed
 
-    window.after(1000,update_v)
-
-a=0
+#function for temperature switching
+a=0#variable "a" which determines if the button has been pressed is a global variable as to allow other functions to know if were in farenheit or celsius
 def swi_temp():
     global a
+    #if the button has just been pressed convert the entire temperature vector to farenheit
     if a == 0:
         for x in range(0,300):
             temp[x]=(temp[x]*1.8)+32
         a=1
-        update()
-
+        update()#update plot and temp reading
+    #if the button is pressed again then change the temperature back to Celsius
     else:
         for x in range(0,300):
             temp[x]=(temp[x]-32)/1.8
         a=0
-        update()
+        update()#update plot and temp reading
     return
 
+#get the first 300 values data points from the server and put them into a vector j
 r = requests.get("https://api.thingspeak.com/channels/" + THINGSPEAK_CHANNELID + "/fields/1.json", params={"api_key":THINGSPEAK_API_READKEY,"results":"300"})
 j = r.json()
 
-enter_v()
+enter_v() #call the 2nd window I created but first window that pops up that allows you to enter your upper temp bound and lower temp bound as well as a phone number to text when the system goes outside those bounds
 
 window = tk.Tk() #create window
 
@@ -276,29 +275,31 @@ button_temp.place(x="450",y="450")
 #initialize temp vector to be an empty vector of 300 spaces to be filled when data is read from the microcontroller
 time_s = [-299]
 temp = []
+
+#make the time vector a vector from -300 to 0 representing the last 300 seconds
 for x in range (1, 300):
     time_s.append(x-299)
 
+#seperate the temperature readings our of the vector j and place themp into the temp vector
 for entry in j["feeds"]:
     if entry["field1"] != None:
         temp.append(float(entry["field1"]))
     else:
         temp.append(0)
 
-ID_value = 0
+ID_value = 0 #initialize the ID_value to be reassigned in the graph function
 
 siz=tkFont.Font(size=30) #size of temperature font in the window
 
 fig = fi.Figure(figsize=(6,4.3)) #size of figure that represents the graph
 
-
-update_g()
+update_g()#update plot
 
 #creating and placing the temperature display value on the button
 temp_=tk.Label(window, text = temp[len(temp)-1], font= siz)
 temp_.place(x="520",y="545")
 
-window.after(1000,update_v)
+window.after(50,update_v)#enter into the update_v function after 1 second
 
 window.mainloop()
 
